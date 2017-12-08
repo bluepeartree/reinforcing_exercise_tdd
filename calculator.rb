e2c156615da6ab6d1b44f73f7e27041ce5621ae6
bluepeartree <sherrie.sebastian@gmail.com>
2017-12-07 20:06:53 -0500

    created calculator methods

---
+++ calculator.rb
@@ -0,0 +1,19 @@
+def add(number_1, number_2)
+  return number_1 + number_2
+end
+
+def subtract(number_1, number_2)
+  return number_1 - number_2
+end
+
+def sum(array)
+  return array.sum
+end
+
+# long version of sum
+
+# def sum(array)
+#   array_sum = 0
+#   array.map {|number| array_sum = array_sum + number}
+#   return array_sum
+# end
